849e2ec062d5682b312d1a4f87f18e5ce0917db8
jhonvidal <jhonvi2@gmail.com>
2024-03-18 15:08:09 -0400

    feat: mappings move line

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,5 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
+vim.g.vscode_snippets_path = vim.fn.stdpath("config") .. "/lua/snips"
 -- add yours here
 
 local map = vim.keymap.set
@@ -6,9 +7,15 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
 map("n", "<leader>fm", function()
-  require("conform").format()
+	require("conform").format()
 end, { desc = "File Format with conform" })
 
 map("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 
-vim.g.vscode_snippets_path = vim.fn.stdpath("config") .. "/lua/snips"
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
